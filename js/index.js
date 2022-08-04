4ee9ed6754706863813499cc0ae1ae775ee4d396
Elena-MyOne <myone72@yahoo.com>
2022-08-04 01:09:55 -0400

    feat: implement date functionality

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 const time = document.querySelector('.time');
+const day = document.querySelector('.date');
 
 setInterval(function () {
 	const date = new Date();
 	const currentTime = date.toLocaleTimeString();
 	time.textContent = currentTime;
+	const options = { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric', timeZone: 'UTC' };
+	const currentDate = date.toLocaleDateString('en-US', options);
+	day.textContent = currentDate;
 }, 1000)
 
